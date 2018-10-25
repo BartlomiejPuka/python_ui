c6cf2d304a2a4736b1bda4e567f4f2630addf913
Javatron <bartomiej.puka@gmail.com>
2018-10-25 21:55:24 +0200

    - updated initUI method - added buttons - added layout boxes

--- msgbox.py
+++ msgbox.py
@@ -1,7 +1,8 @@
 import sys
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtWidgets import QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QLabel
 
-class MsgBox(QtWidgets.QDialog):
+class MsgBox(QtWidgets.QWidget):
     def __init__(self, prompt="prompt", title="title"):
         """
         param::prompt describes what u want
@@ -11,17 +12,59 @@ class MsgBox(QtWidgets.QDialog):
         self.title = title
         self.left = 50
         self.top = 50
-        self.width = 400
-        self.height = 140
+        self.width = 280
+        self.height = 120
         self.prompt = prompt
 
-        self.initUI()
+        
+        self.__initUI()
 
-    def initUI(self):
+
+    def __initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        self.center()
+        self.label = QLabel("promptaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", self)
+        
+        self.label.adjustSize()
+
+        self.inputLine = QLineEdit(self)
+        self.inputLine.setFixedHeight(25)
+
+        self.okButton = QPushButton("Ok",self)
+        self.cancelButton = QPushButton("Cancel",self)
+        self.showButton = QPushButton("Show",self)
+
+        self.labelBox = QHBoxLayout()
+        self.labelBox.setAlignment(QtCore.Qt.AlignCenter)
+        self.labelBox.addWidget(self.label)
+
+        self.inputLineBox = QHBoxLayout()
+        self.labelBox.setAlignment(QtCore.Qt.AlignCenter)
+        self.inputLineBox.addWidget(self.inputLine)
+
+
+
+        self.buttonsBox = QHBoxLayout()
+        self.buttonsBox.addWidget(self.okButton)
+        self.buttonsBox.addStretch(1)
+        self.buttonsBox.addWidget(self.showButton)
+        self.buttonsBox.addStretch(2)
+        self.buttonsBox.addWidget(self.cancelButton)
+
+
+        self.vbox = QVBoxLayout(self)
+        self.vbox.addLayout(self.labelBox)
+        self.vbox.addStretch(1)
+        self.vbox.addLayout(self.inputLineBox)
+        self.vbox.addStretch(1)
+        self.vbox.addLayout(self.buttonsBox)
+        
+        self.setLayout(self.vbox)
+
+
+        self.setFixedSize(self.size())
+        self.__center()
         self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
         self.show()
 
@@ -31,16 +74,13 @@ class MsgBox(QtWidgets.QDialog):
         msgbox = MsgBox(prompt,title)
         app.exec_()
 
-    def center(self):
+    def __center(self):
         # geometry of the main window
         qr = self.frameGeometry()
-
         # center point of screen
         cp = QtWidgets.QDesktopWidget().availableGeometry().center()
-
         # move rectangle's center point to screen's center point
         qr.moveCenter(cp)
-
         # top left of rectangle becomes top left of window centering it
         self.move(qr.topLeft())
     
